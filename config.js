4d36ec63b99b1707af4288a5d4b009e906b6a5d6
DangChucc <98172550+DangChucc@users.noreply.github.com>
2022-01-21 22:15:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Mừng ngày kỉ niệm",
+    introTitle: 'Nguyễn Trà My',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
